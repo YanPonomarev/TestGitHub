b3c44894e1ea12f607b40600efe5bf854146c39e
Yan Ponomarev <yanponomarev@gmail.com>
2021-08-05 15:22:36 +0300

    Пробуем коммитить TODO
    Добавили TODO

--- Test1.py
+++ Test1.py
@@ -3,9 +3,13 @@ from statistics import geometric_mean
 import numpy as np
 
 from PIL.Image import Image
+# обычный коммент
 
 library = np.array([[3], [1], [0]])
 message_1 = 'Привет Git'
+# todo Поглядим как работает
+#   Вторая линия туду
+
 
 
 message_2 = 'Привет отец'
